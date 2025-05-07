c983993088ad0483e00f1970910cd71da526aaa9
Maxence-Esteban CHATEAU <maxence-esteban.chateau@etu.umontpellier.fr>
2025-05-07 14:42:29 +0200

    Modifs pour la gestion des signaux + ajouts de commentaires

--- client.c
+++ client.c
@@ -1,101 +1,175 @@
-#include <stdio.h>
-#include <stdlib.h>
+// ------ Importation des Librairies ------
 #include <sys/socket.h>
 #include <netinet/in.h> 
-#include <arpa/inet.h>
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <sys/wait.h>
 
+
+// ------  Variables globales ------
+#define SERVER_IP   "127.0.0.1"         
+#define SERVER_PORT 12345
+#define BUFFER_MAX    1024
+
+
+// ------  Fonction de gestions des signaux ------
+static void handleSigint(int sig) {
+    (void)sig;
+    printf("\nArrêt client\n");
+    exit(EXIT_SUCCESS);
+}
+
+// Fonction de creation de la socket UDP
 int createSocket()
 {
-	return socket(PF_INET, SOCK_DGRAM, 0);
+    return socket(PF_INET, SOCK_DGRAM, 0);
+
 }
 
+// Gestion du port utilise sur le programme 
 struct sockaddr_in *adServ(int port)
 {
-	struct sockaddr_in *adServer = malloc(sizeof(struct sockaddr_in));
-	adServer->sin_family = AF_INET; 
-	adServer->sin_port = htons(port);
+    struct sockaddr_in *adServer = malloc(sizeof(struct sockaddr_in));
+    adServer->sin_family = AF_INET; 
+    adServer->sin_port = htons(port);
 
-	return adServer;
+    return adServer;
 }
 
+// Fonction de connexion au serveur
 int connection(int ds, char *ip, struct sockaddr_in *adServ)
 {
-	int res = inet_pton(AF_INET, ip, &(adServ->sin_addr));
-	socklen_t lgA = sizeof(struct sockaddr_in);
+    int res = inet_pton(AF_INET, ip, &(adServ->sin_addr));
+    socklen_t lgA = sizeof(struct sockaddr_in);
 
-	res = connect(ds, (struct sockaddr *) adServ, lgA);
+    res = connect(ds, (struct sockaddr *) adServ, lgA);
 
-	return res;
+    return res;
 }
 
+// Verification de la connexion
 void debugConnexion(int res)
 {
-	if (res == 0)
-		printf("ouais c'est connecte\n");
-	else
-		printf("nope dommage\n");
+    if (res == 0)
+        printf("ouais c'est connecte\n");
+    else
+        printf("nope dommage\n");
 
-	return ;
+    return ;
 }
 
-
+/* Fonction d'envoi d'un message
+ *var dS :          Socket de connexion
+ *var adServer :    Serveur destinataire
+ *var message :     message formalise au prealable
+*/
 ssize_t sendMessage(int dS, struct sockaddr_in *adServer, char *message)
 {
-	socklen_t lgA = sizeof(struct sockaddr_in);
-	return sendto(dS, message, 1024, 0, (struct sockaddr *)adServer, lgA);
+    socklen_t lgA = sizeof(struct sockaddr_in);
+    return sendto(dS, message, 1024, 0, (struct sockaddr *)adServer, lgA);
 }
 
+// Fonction de debug d'envoi d'un message
 void debugSendMessage(ssize_t snd)
 {
-	if (snd == -1)
-		perror("Erreur envoi msg : ");
+    if (snd == -1)
+        perror("Erreur envoi msg : ");
 
-	return ;
+    return ;
 }
 
+/*  Fonction de formalisation de message
+    *var ip :       adresse ip de l'expediteur
+    *var dest :     pseudo du destinataire
+    *var msg :      Message a envoyer 
+
+    *return : message formalise ("ip//dest//numero msg//nb total message/#/#00message")
+*/
 char *createMessage(char *ip, char *dest, char *msg)
 {
-	char *message = malloc(1024*sizeof(char));
-	
-	strcpy(message, ip);
-	strcat(message, "//");
+    char *message = malloc(1024*sizeof(char));
+    
+    strcpy(message, ip);
+    strcat(message, "//");
 
-	strcat(message, dest);
-	strcat(message, "//");
+    strcat(message, dest);
+    strcat(message, "//");
 
-	strcat(message, "1");
-	strcat(message, "//");
+    strcat(message, "1");
+    strcat(message, "//");
 
-	strcat(message, "1");
-	strcat(message, "/#/#00");
+    strcat(message, "1");
+    strcat(message, "/#/#00");
 
-	strcat(message, msg);
+    strcat(message, msg);
 
-	return message;
+    return message;
 }
 
-int main()
-{
-	int dS = createSocket();
-	struct sockaddr_in *adServer = adServ(34567);
 
-	int res = connection(dS, "127.0.0.1", adServer);
-	debugConnexion(res);
+int main(void) {
+    signal(SIGINT, handleSigint);
+    int dS = createSocket();
+    if (dS < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);         // Cas d'erreur 
+    }
+    struct sockaddr_in *adServer = adServ(SERVER_PORT);
 
-	char messagesend[1024] = "Ouais ouais ouais !";
-	char *message = createMessage("127.0.0.1", "nabilb", messagesend);
+    if ( connection(dS, SERVER_IP, adServer) != 0 ) {
+        debugConnexion(-1);
+        exit(EXIT_FAILURE);         // Cas d'erreur lors de la connexion
+    }
+    debugConnexion(0);
 
-	ssize_t snd = sendMessage(dS, adServer, message);
-	debugSendMessage(snd);
-	
-	char messagercv[1024];
+    pid_t pid = fork();             // Creation de processus
+    if (pid < 0) {
+        perror("fork"); 
+        exit(EXIT_FAILURE);
+    }
 
-	recvfrom(dS, messagercv, strlen(messagercv), 0, NULL, NULL);
-	printf("reponse : %s \n", messagercv);
+    if (pid == 0) {
+        // ── Processus enfant : réception bloquante des ACKs ──
+        char buf[BUFFER_MAX];
+        while (1) {
+            ssize_t r = recvfrom(dS, buf, BUFFER_MAX-1, 0, NULL, NULL);
+            if (r < 0) {
+                perror("recvfrom"); 
+                _exit(EXIT_FAILURE);
+            }
+            buf[r] = '\0';
+            write(STDOUT_FILENO, "\n ", 3);
+            write(STDOUT_FILENO, buf, r);
+            write(STDOUT_FILENO, "\n> ", 3);
+        }
+    } else {
+        // ── Processus parent : envoi bloquant via read() ──
+        char line[BUFFER_MAX];
+        write(STDOUT_FILENO, "> ", 2);
 
-	close(dS);
+        ssize_t n;
+        while ((n = read(STDIN_FILENO, line, BUFFER_MAX-1)) > 0) {
+            if (line[n-1] == '\n') n--;
+            line[n] = '\0';
 
-	return 0;
+            // construction et envoi du message structuré
+            char *message = createMessage(SERVER_IP, "destPseudo", line);
+            ssize_t s = sendMessage(dS, adServer, message);
+            debugSendMessage(s);
+            free(message);
+
+            write(STDOUT_FILENO, "> ", 2);
+        }
+
+        kill(pid, SIGTERM);
+        wait(NULL);
+        close(dS);
+        free(adServer);
+    }
+
+    return EXIT_SUCCESS;
 }
